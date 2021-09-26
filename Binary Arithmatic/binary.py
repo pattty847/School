d28ea99d11cfce1176b1b4a35dd41f0d7305507d
Patrick McDermott <pattty847@gmail.com>
2021-09-25 22:28:44 -0400

    Small changes

---
+++ Binary Arithmatic/binary.py
@@ -0,0 +1,25 @@
+
+
+
+
+result = []
+def to_binary(n):
+    if n >= 1:
+        to_binary(n // 2)
+    print(n % 2, end="")
+
+
+
+def binary(n):
+    result = []
+    while n >= 1:
+        n / 2
+
+
+
+if __name__ == '__main__':
+
+    value = 17
+
+    to_binary(value)
+
--- crypto watcher/terminal.py
+++ crypto watcher/terminal.py
@@ -9,5 +9,18 @@ cg = CoinGeckoAPI()
 coins = ["bitcoin", "ethereum", "solana", "avalance"]
 
 
-data = cg.get_search_trending()
+trending_data = cg.get_search_trending()
+trending_coins = []
+coins_prices = []
+for coins in trending_data["coins"]:
+    price = cg.get_price(ids=coins["item"]["id"], vs_currencies='usd')
+    trending_coins.append(coins["item"]["id"] + ":"+price)
+    
 
+
+with dpg.window(label="Trending Coins", id="terminal"):
+    for each in trending_coins:
+        dpg.add_text(each)
+
+
+dpg.start_dearpygui()
