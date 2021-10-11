2dd3b6110274aeda5e697c2679f7d737229889a8
Patrick McDermott <pattty847@gmail.com>
2021-10-10 23:36:48 -0400

    Comments upload

---
+++ Exaustion Suite/config.py
@@ -0,0 +1,3 @@
+api_key = "6162fd36bc85c200065b10ea"
+api_secret = "5815f4e7-d387-455a-8edb-d5b0a36320c0"
+api_passphrase = "61619d9942702600065eeb6d"
---
+++ Exaustion Suite/wsgi.py
@@ -0,0 +1,105 @@
+import json, config
+from flask import Flask, request, abort
+from flask.templating import render_template
+from flask import Flask, request, redirect, url_for
+from kucoin.client import Market, Trade, User
+
+# Initialize our Flask application server
+app = Flask(__name__)
+
+# route() is a decorator which binds functions to URLs
+# /index is the home page for the website
+@app.route('/index')
+def index():
+
+    return render_template("index.html")
+
+# /crypto displays the TradeSuite page
+@app.route('/crypto')
+def crypto():
+
+    return render_template("crypto.html")
+
+# this is a redirect to the index if the user goes to the root of the website
+@app.route('/')
+def redirectIndex():
+
+    return redirect(url_for("index"))
+
+# /webhook is where our Tradingview scripts will send json data
+@app.route('/webhook', methods=['POST'])
+def webhook():
+
+    # loads the requests data in json format
+    data = json.loads(request.data)
+
+    # store the output of read_alert() which is our orderId
+    response = read_alert(data)
+
+    # returns to webhook success or failure and the orderId if successful
+    return {
+        "code": "sucess",
+        "message": response
+    }
+
+# API Information
+# USDT ID: 61619d9942702600065eeb6d
+api_key = config.api_key
+api_secret = config.api_secret
+api_passphrase = config.api_passphrase
+
+client = Trade(api_key, api_secret, api_passphrase, is_sandbox=True)
+user = User(api_key, api_secret, api_passphrase, is_sandbox=True)
+
+# Stores the information in USDT wallet
+balance = user.get_account("61619d9942702600065eeb6d")
+
+# This list will store all of our previous orders so we can refer to them later
+orders_list = []
+
+# Function to place limit orders and add that order to the list of orders
+def limit_order(symbol, side, size, price):
+    price = int(float(price))
+    order = client.create_limit_order(symbol, side, size, str(price))
+    ladderTakeProfit(order)
+
+    orders_list.append(client.get_order_details(order['orderId']))
+
+    return order
+
+# Function to place market orders and add that order to the list of orders
+def market_order(symbol, side, size):
+    order = client.create_market_order(symbol, side, size)
+
+    orders_list.append(client.get_order_details(order['orderId']))
+
+    return order
+
+def ladderTakeProfit(orderId, chunks):
+    entry = orderId['price']
+    print(entry)
+
+# Main read function that interprets webhook alerts
+def read_alert(data):
+    if(data['type'] == "market"):
+
+        order = market_order(symbol=data['symbol'], side=data['side'], size=data['size'])
+
+    elif(data['type'] == "limit"):
+
+        order = limit_order(symbol=data['symbol'], side=data['side'], size=data['size'], price=data['price'])
+
+    elif(data['type'] == "cancel"):
+        
+        order = client.cancel_all_orders()
+
+    else:
+
+        order = "Incorrect 'type' format. Please use 'market' or 'limit'."
+
+    return order
+    
+
+
+if __name__ == "__main__":
+    app.run()
