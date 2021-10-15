8ae911ab71483c9e3be1439937541e2932098283
Patrick McDermott <pattty847@gmail.com>
2021-10-15 12:28:28 -0400

    Asset Patrol
    Started working on how to pull and display information from an API to a GUI. I used DearPyGUI and am just getting started learning it.

--- Asset Patrol/main.py
+++ Asset Patrol/main.py
@@ -2,7 +2,9 @@ import numpy as np
 import pandas as pd
 import dearpygui.dearpygui as dpg
 from pycoingecko import CoinGeckoAPI
-
+from pprint import pprint
+import pandas as pd
+import datetime
 
 
 cg = CoinGeckoAPI()
@@ -12,28 +14,49 @@ vp = dpg.create_viewport(title='TradeSuite', width=1600, height=1200)
 dpg.setup_dearpygui(viewport=vp)
 
 
-def save_callback():
-    print("Save Clicked")
+def getOHLC(sender, app_data, user_data):
 
-def get_input(sender, app_data, user_data):
-    dpg.set_value('results', cg.get_price(dpg.get_value('input'), vs_currencies='usd'))
+    btc_OHLC = cg.get_coin_ohlc_by_id(dpg.get_value('input'), vs_currency='usd', days=90)
+    btc_df = pd.DataFrame(btc_OHLC)
+    btc_df.rename(columns={0: 'volume', 1: 'open', 2: 'high', 3: 'low', 4: 'close'}, inplace=True)  
+    with dpg.table(header_row=True, parent='candlestick-chart'):
+
+            # use add_table_column to add columns to the table,
+            # table columns use child slot 0
+            dpg.add_table_column(label="Volume")
+            dpg.add_table_column(label="Open")
+            dpg.add_table_column(label="High")
+            dpg.add_table_column(label="Low")
+            dpg.add_table_column(label="Close")
+            for i in range(0, btc_df.shape[0]):
+                for j in range(0, btc_df.shape[1]):
+                    dpg.add_text(btc_df.iloc[i, j])
+
+                    # call if not last cell
+                    if not (i == btc_df.shape[0] and j == btc_df.shape[1]):
+                        dpg.add_table_next_column()
+
+
+            # dpg.add_candle_series(dates=list(btc_df.index), opens=btc_df['open'], highs=btc_df['high'], lows=['low'], closes=['close'])
 
 # TradeSuite homepage
 def start_viewport():
-    with dpg.window(id = "candlestick-chart", label="TradeSuite", width=500, height=500):
+    with dpg.window(id = "candlestick-chart", label="TradeSuite"):
         dpg.add_input_text(id="input", label='Enter a Crypto')
-        dpg.add_button(label="Search Coin Price", callback=get_input)
-        dpg.add_text(id='results')
-        dpg.add_table()
-
+        dpg.add_button(label="Search Coin Price", callback=getOHLC)
+        dpg.add_table(id='results')
         
 
+        
 
 
 
 if(__name__ == "__main__"):
     start_viewport()
     dpg.show_viewport(vp)
+    dpg.set_primary_window('candlestick-chart', True)
+    dpg.show_implot_demo()
+    dpg.show_imgui_demo()
     # below replaces, start_dearpygui()
     while dpg.is_dearpygui_running():
         # insert here any code you would like to run in the render loop
