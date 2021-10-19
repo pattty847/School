a795d42a3a9f0844ba2e9a4c50667408e5bcec34
Patrick McDermott <pattty847@gmail.com>
2021-10-18 20:00:46 -0400

    Asset Patrol
    Added portfolio index manager

--- Asset Patrol/main.py
+++ Asset Patrol/main.py
@@ -1,3 +1,4 @@
+from json import load
 import numpy as np
 import pandas as pd
 import dearpygui.dearpygui as dpg
@@ -12,7 +13,8 @@ btc = cg.get_coin_ohlc_by_id('bitcoin', vs_currency='usd', days=90)
 df = pd.DataFrame(btc)
 df.rename(columns={0: 'unix', 1: 'open', 2: 'high', 3: 'low', 4: 'close'}, inplace=True)  
 df['date'] = pd.to_datetime(df['unix'], unit='ms')
-
+coins = []
+portfolio = pd.DataFrame(columns=['Coins', 'Shares', 'Purchase Price', 'Initial Investment'])
 
 # Main program viewport
 vp = dpg.create_viewport(title='TradeSuite', width=1600, height=1200)
@@ -21,32 +23,33 @@ dpg.setup_dearpygui(viewport=vp)
 
 def getOHLC(sender, app_data, user_data):
     search_value = dpg.get_value('input')
-    if(search_value != ""):
-        btc_OHLC = cg.get_coin_ohlc_by_id(search_value, vs_currency='usd', days=90)
-        btc_df = pd.DataFrame(btc_OHLC)
-        btc_df.rename(columns={0: 'volume', 1: 'open', 2: 'high', 3: 'low', 4: 'close'}, inplace=True)  
-        with dpg.table(header_row=True, parent='candlestick-chart'):
+    if(search_value == ""): return ""
+    
+    btc_OHLC = cg.get_coin_ohlc_by_id(search_value, vs_currency='usd', days=90)
+    btc_df = pd.DataFrame(btc_OHLC)
+    btc_df.rename(columns={0: 'volume', 1: 'open', 2: 'high', 3: 'low', 4: 'close'}, inplace=True)  
+    with dpg.table(header_row=True, parent='candlestick-chart'):
 
-                # use add_table_column to add columns to the table,
-                # table columns use child slot 0
-                dpg.add_table_column(label="Volume")
-                dpg.add_table_column(label="Open")
-                dpg.add_table_column(label="High")
-                dpg.add_table_column(label="Low")
-                dpg.add_table_column(label="Close")
-                for i in range(0, btc_df.shape[0]):
-                    for j in range(0, btc_df.shape[1]):
-                        dpg.add_text(btc_df.iloc[i, j])
+            # use add_table_column to add columns to the table,
+            # table columns use child slot 0
+            dpg.add_table_column(label="Volume")
+            dpg.add_table_column(label="Open")
+            dpg.add_table_column(label="High")
+            dpg.add_table_column(label="Low")
+            dpg.add_table_column(label="Close")
+            for i in range(0, btc_df.shape[0]):
+                for j in range(0, btc_df.shape[1]):
+                    dpg.add_text(btc_df.iloc[i, j])
 
-                        # call if not last cell
-                        if not (i == btc_df.shape[0] and j == btc_df.shape[1]):
-                            dpg.add_table_next_column()
+                    # call if not last cell
+                    if not (i == btc_df.shape[0] and j == btc_df.shape[1]):
+                        dpg.add_table_next_column()
 
 
             # dpg.add_candle_series(dates=list(btc_df.index), opens=btc_df['open'], highs=btc_df['high'], lows=['low'], closes=['close'])
 
-# TradeSuite homepage which is called after the __main__ python function runs
-def start_viewport():
+
+def draw_candleplot():
     for each in df['date']:
         print(each)
 
@@ -77,6 +80,68 @@ def start_viewport():
 
             dpg.add_candle_series(dates=dates, opens=opens, highs=highs, lows=lows, closes=closes, label="Candlesticks", parent="plot")
 
+def load_portfolio():
+    with dpg.window(id = 'portfolio', label='Portfolio', width=800, height=800, no_resize=True):
+        # create plot
+        with dpg.plot(label="Line Series", height=400, width=400):
+
+            # optionally create legend
+            dpg.add_plot_legend()
+
+            # REQUIRED: create x and y axes
+            dpg.add_plot_axis(dpg.mvXAxis, label="x")
+            dpg.add_plot_axis(dpg.mvYAxis, label="y", id="y_axis")
+
+            dpg.add_pie_series(x=10.0, y=40.0, radius=50.0, values=['itemdsas', 'dsfasd', 'sfsadfa'], labels=['fdsa', 'sdfas', 'fdsafs'], parent=dpg.last_item())
+
+def add_coin():
+    if(dpg.get_value('crypto-index') == "" or cg.get_price(dpg.get_value('crypto-index'), vs_currencies='usd') == {}): return print("Error finding coin.")
+
+    coins.append(dpg.get_value('crypto-index'))
+    print('added')
+
+def create_portfolio():
+    portfolio['Coins'] = coins
+    for each in portfolio['Coins']:
+        print(each)
+    return portfolio
+
+def create_index():
+    with dpg.window(id='create-index', label='Create Index', width=800, height=800, no_resize=True):
+        # input text box where we grab a cryptocurrency from the user
+        dpg.add_input_text(id="crypto-index", label='Enter a Crypto')
+        dpg.add_button(label="Add Coin", callback=add_coin)
+        dpg.add_button(label="Done", callback=create_portfolio)
+
+# TradeSuite homepage which is called after the __main__ python function runs
+def start_viewport():
+    # We create the Primary Window called 'candlestick-chart' which we can push information to later using parent
+    with dpg.window(id = "main-viewport", label="TradeSuite"):
+
+        with dpg.menu_bar():
+            
+            with dpg.menu(label='View'):
+                with dpg.menu(label='Crypto'):
+                    dpg.add_menu_item(label='Portfolio', callback=load_portfolio)
+                    dpg.add_menu_item(label='Create Index', callback=create_index)
+                    dpg.add_menu_item(label='Ideas')
+                    dpg.add_menu_item(label='Market')
+
+                with dpg.menu(label='Stocks'):
+                    dpg.add_menu_item(label='Portfolio')
+                    dpg.add_menu_item(label='Charts')
+                    dpg.add_menu_item(label='Ideas')
+                    dpg.add_menu_item(label='Market')
+
+            with dpg.menu(label='File'):
+                dpg.add_menu_item(label='Item 1')
+            with dpg.menu(label='File'):
+                dpg.add_menu_item(label='Item 1')
+
+
+        # input text box where we grab a cryptocurrency from the user
+        dpg.add_input_text(id="input", label='Enter a Crypto')
+        dpg.add_button(label="Search Coin Price", callback=getOHLC)
 
 
 
@@ -84,9 +149,9 @@ def start_viewport():
 if(__name__ == "__main__"):
     start_viewport()
     dpg.show_viewport(vp)
-    dpg.set_primary_window('candlestick-chart', True)
-    dpg.show_implot_demo()
-    dpg.show_imgui_demo()
+    dpg.set_primary_window('main-viewport', True)
+    # dpg.show_implot_demo()
+    # dpg.show_imgui_demo()
     # below replaces, start_dearpygui()
     while dpg.is_dearpygui_running():
         # insert here any code you would like to run in the render loop
