0a60b336886972acda4f3a65cd33c7cc23de95aa
Patrick McDermott <pattty847@gmail.com>
2022-01-10 12:51:51 -0500

    upload

--- Coinalyze Scraper/login.py
+++ Coinalyze Scraper/login.py
@@ -6,6 +6,9 @@ from ta import add_all_ta_features
 from ta.utils import dropna
 import math
 from configparser import ConfigParser
+import numpy as np
+import matplotlib.pyplot as plt
+import json
 
 
 # from variable id
@@ -35,32 +38,16 @@ gate = exchange_class({
     wtMALen = input(3, title = 'WT MA Length', type = input.integer)
     [wt1, wt2, wtOversold, wtOverbought, wtCross, wtCrossUp, wtCrossDown, wtCrosslast, wtCrossUplast, wtCrossDownlast, wtVwap]
 """
-
 # This function will calculate the WaveTrend and return them
-""" Wave Trend:
-    tfsrc = security(syminfo.tickerid, tf, src)
-    esa = ema(tfsrc, chlen)
-    de = ema(abs(tfsrc - esa), chlen)
-    ci = (tfsrc - esa) / (0.015 * de)
-    wt1 = security(syminfo.tickerid, tf, ema(ci, avg))
-    wt2 = security(syminfo.tickerid, tf, sma(wt1, malen))
-    wtVwap = wt1 - wt2
-    wtOversold = wt2 <= osLevel
-    wtOverbought = wt2 >= obLevel
-    wtCross = cross(wt1, wt2)
-    wtCrossUp = wt2 - wt1 <= 0
-    wtCrossDown = wt2 - wt1 >= 0
-    wtCrosslast = cross(wt1[2], wt2[2])
-    wtCrossUplast = wt2[2] - wt1[2] <= 0
-    wtCrossDownlast = wt2[2] - wt1[2] >= 0
-"""
-def calculateWaveTrend(coin, timeframe, chlen, avg, malen, oslevel, oblevel):
+def calculateWaveTrend(coin, timeframe, chlen, avg, malen, oslevel, oblevel, history):
     # Load the data from 'coin' by 'timeframe' (1000 lines 0-999)
     tfSrc = pd.DataFrame(gate.fetch_ohlcv(coin, timeframe))
+
     # this will check to see if the exchange sent the last minute. Sometimes the tfSrc DataFrame will only give 999/1000 rows
     # we need to check the last index to make sure it's 999.
-    while(tfSrc.index[-1] != 999):
-        tfSrc = pd.DataFrame(gate.fetch_ohlcv(coin, timeframe))
+    # while(tfSrc.index[-1] != 999):
+    #    tfSrc = pd.DataFrame(gate.fetch_ohlcv(coin, timeframe))
+
     tfSrc = dropna(tfSrc)
     tfSrc.rename(columns= {
         0: 'Time', 
@@ -70,9 +57,10 @@ def calculateWaveTrend(coin, timeframe, chlen, avg, malen, oslevel, oblevel):
         4: 'Close',
         5: 'Volume'}, inplace=True, errors='raise')
 
+    # Calculate the HLC (high low close) mean
     tfSrc['HLC3'] = (tfSrc['High'] + tfSrc['Low'] + tfSrc['Close']) / 3
 
-    # EMA = ( 2/n + 1 ) * (Close-Previous_EMA) + Previous_EMA
+    # ESA = Exponential Moving Average
     tfSrc['ESA'] = tfSrc['HLC3'].ewm(span=chlen, adjust=False).mean()
 
     # de = ema(abs(tfsrc - esa), chlen)
@@ -96,46 +84,41 @@ def calculateWaveTrend(coin, timeframe, chlen, avg, malen, oslevel, oblevel):
     tfSrc['wtCrosslast'] = tfSrc['wt1'][2] >= tfSrc['wt2'][2]
     tfSrc['wtCrossUplast'] = tfSrc['wt2'][2] - tfSrc['wt1'][2] <= 0
     tfSrc['wtCrossDownlast'] = tfSrc['wt2'][2] - tfSrc['wt1'][2] >= 0
-    
-    # return the last minute
-    return tfSrc[-1:]
 
-def checkForTrade(wt):
     # Buy signal.
-    buySignal = wt['wtCross'] and wt['wtCrossUp'] and wt['wtOversold']
-
-    # buySignalDiv = (wtwtShowDiv and wtBullDiv) or 
-    #            (wtShowDiv and wtBullDiv_add) or 
-    #            (stochShowDiv and stochBullDiv) or 
-    #            (rsiShowDiv and rsiBullDiv)
-        
-    # buySignalDiv_color = wtBullDiv ? colorGreen : 
-    #                    wtBullDiv_add ? color.new(colorGreen, 60) : 
-    #                    rsiShowDiv ? colorGreen : na
+    tfSrc['Buy'] = tfSrc['wtCross'].all() and tfSrc['wtCrossUp'].all() and tfSrc['wtOversold'].all()
 
     # Sell signal
-    sellSignal = wt['wtCross'] and wt['wtCrossDown'] and wt['wtOverbought']
-                
-    # sellSignalDiv = (wtShowDiv and wtBearDiv) or 
-    #            (wtShowDiv and wtBearDiv_add) or
-    #            (stochShowDiv and stochBearDiv) or
-    #            (rsiShowDiv and rsiBearDiv)
-                        
-    # sellSignalDiv_color = wtBearDiv ? colorRed : 
-    #                    wtBearDiv_add ? color.new(colorRed, 60) : 
-    #                    rsiBearDiv ? colorRed : na
+    tfSrc['Sell'] = tfSrc['wtCross'].all() and tfSrc['wtCrossDown'].all() and tfSrc['wtOverbought'].all()
 
-    # Gold Buy 
-    # lastRsi = valuewhen(wtFractalBot, rsi[2], 0)[2]
-    # wtGoldBuy = ((wtShowDiv and wtBullDiv) or (rsiShowDiv and rsiBullDiv)) and
-    #        wtLow_prev <= osLevel3 and
-    #        wt2 > osLevel3 and
-    #        wtLow_prev - wt2 <= -5 and
-    #        lastRsi < 30
-    if(buySignal):
+    # return the last minute
+    return tfSrc if history else tfSrc[-1:]
+
+def checkForTrade(wt, coin, trade_ratio):
+    # This will grab the orderbook for the coin
+    orderbook = gate.fetch_order_book(coin)
+
+    # trade size is the account balance * the trade ratio
+    trade_size = gate.fetch_balance()[coin.split('/')[1]]['free'] * trade_ratio
+
+
+    if(wt.Buy.all() == True):
         pass
+        # limit_order_placement =  gate.create_limit_buy_order(coin, '200', '0.013')
 
+    print(trade_size)
+    pprint(orderbook)
 
 if __name__ == "__main__":
     # calculateWaveTrend(coin, timeframe, chlen, avg, malen, oslevel, oblevel)
-    print(calculateWaveTrend("BTC/USDT", '1m',  9, 12, 3, -53, 53))
+
+    coin = 'BTC'
+    currency = 'USDT'
+    pair = coin+'/'+currency
+    timeframe = '1m'
+    trade_ratio_to_balance = '.1' # 10% of account balance to be used / trade
+
+    waveTrend = calculateWaveTrend(pair, timeframe,  9, 12, 3, -53, 53, True)
+    lastMinuteWT = calculateWaveTrend(pair, timeframe,  9, 12, 3, -53, 53, False)
+
+    checkForTrade(lastMinuteWT, pair, trade_ratio_to_balance)
