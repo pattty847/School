c0f3ccf4c259bd2b0b50263fad56cb1727132010
Patrick McDermott <pattty847@gmail.com>
2022-09-26 18:11:45 -0400

    Removed some files

--- SQL/DatabaseManagement/Week 1/DB Basic SELECT with AND/run.sql
+++
@@ -1,5 +0,0 @@
-SELECT 1 = 0 AND 1 / 0;
-
-SELECT customername, country, state FROM customers WHERE country='USA' AND state='CA';
-
-SELECT customername, country, state, creditlimit FROM customers WHERE country='USA' AND state='CA' AND creditlimit > 100000;
--- SQL/DatabaseManagement/Week 1/DB Basic Single Table Queries/run.sql
+++
@@ -1,9 +0,0 @@
-SELECT id, street, value, zip FROM house WHERE zip = '19140';
-
-SELECT id, street, value, zip, bedrooms, bathrooms FROM house WHERE zip = '19128' AND bedrooms >= 3 AND bathrooms=2.0;
-
-SELECT id, street, zip, value, first, last, sold FROM house WHERE first='Mark' AND last='Jones' AND sold=TRUE;
-
-SELECT street, value, bedrooms, bathrooms, zip FROM house WHERE zip='19116' OR zip='19154';
-
-SELECT id, street, value, zip, sold FROM house WHERE zip='19139' AND sold=FALSE;
